0e8e730bca33af72c98107bbbb8b046ca6fe2869
anuragchavhan0 <anuragchavhan0@localhost>
2025-12-17 16:26:25 +0530

    Demo1

---
+++ PCE/src/com/demo/Demo1.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 45;
+		System.out.println(a);
+		int b = 045;
+		System.out.println(b);
+		int c = 0x45;
+		System.out.println(c);
+		int d = 0b1011;
+		System.out.println(d);
+		
+		
+
+	}
+
+}
---
+++ PCE/src/com/demo/range.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class range {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 128 ;
+		System.out.println(a);
+
+	}
+
+}
