8b03ce51a3498aa99bf286b0344b06ba63e64783
anuragchavhan0 <anuragchavhan0@localhost>
2026-01-01 16:36:43 +0530

    Rules of abstraction & inheritence .

---
+++ PCE/src/com/abstraction/BirdApp.java
@@ -0,0 +1,23 @@
+package com.abstraction;
+
+
+// Rule no 1)
+//1. We cannot instantiate an abstract class . We cannot create an object of abstract class.
+
+
+abstract class Sparrow{
+	abstract void eat();
+	void fly() {
+		System.out.println("Sparrow is flaying ");
+	}
+}
+
+public class BirdApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Sparrow s = new Sparrow();  cannot create a object.
+
+	}
+
+}
---
+++ PCE/src/com/abstraction/BirdAppRule2.java
@@ -0,0 +1,22 @@
+package com.abstraction;
+
+
+
+
+//2 . An Abstract may or may not have abstract methods.
+
+abstract class sparrow{
+	abstract void eat();
+	void fly() {
+		System.out.println("Sparrow is flaying ");
+	}
+}
+
+public class BirdAppRule2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
