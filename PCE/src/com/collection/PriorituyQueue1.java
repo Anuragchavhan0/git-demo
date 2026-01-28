234bed913140b72ae85a1b3f663be1354c7e8749
anuragchavhan0 <anuragchavhan0@localhost>
2026-01-28 14:49:29 +0530

    .

---
+++ PCE/src/com/collection/PriorituyQueue1.java
@@ -0,0 +1,24 @@
+package com.collection;
+
+import java.util.PriorityQueue;
+
+public class PriorituyQueue1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PriorityQueue pq = new PriorityQueue();
+		pq.add(100);
+		pq.add(50);
+		pq.add(150);
+		pq.add(25);
+		pq.add(75);
+		pq.add(125);
+		pq.add(175);
+		System.out.println(pq);
+		
+		
+		
+	}
+
+}
