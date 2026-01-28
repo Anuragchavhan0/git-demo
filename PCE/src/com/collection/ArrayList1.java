02c350d28d94badfc3fc57fda1b2db4803fd27e4
anuragchavhan0 <anuragchavhan0@localhost>
2026-01-28 12:24:23 +0530

    28/01/2026

--- PCE/src/com/collection/ArrayList1.java
+++ PCE/src/com/collection/ArrayList1.java
@@ -16,7 +16,22 @@ public class ArrayList1 {
 		
 		System.out.println(al);
 		
-
+		ArrayList al1 = new ArrayList();
+		al1.add(10);
+		al1.add("hfhu");
+		al1.add(30);
+		al1.add(21.8);
+		al1.add('A');
+		System.out.println(al1);
+		
+		al.add(2, al1);
+		
+		System.out.println(al);
+		System.out.println(al.contains(2));
+		System.out.println(al.containsAll(al1));
+		al.remove(3);
+		al.set(2, 78);
+		System.out.println(al);
 	}
 
 }
